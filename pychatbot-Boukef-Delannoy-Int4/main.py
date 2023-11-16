9e01ff286e53ae35bdfa1d3daafa4b068cefb0d3
DelannoyArthur <129661011+DelannoyArthur@users.noreply.github.com>
2023-11-16 16:22:22 +0100

    Update main.py

---
+++ pychatbot-Boukef-Delannoy-Int4/main.py
@@ -0,0 +1,5 @@
+from function import*
+
+if __name__ == '__main__':
+    file = list_of_files(r"C:\Users\MAC ARTHUR\PycharmProjects\pychatbot-Boukef-Delannoy-Int4-main\pychatbot-Boukef-Delannoy-Int4-main\speeches", ".txt")
+    print(file)
