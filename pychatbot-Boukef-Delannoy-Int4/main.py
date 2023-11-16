92c4dde09635d7d03a87bfa007a405e639125251
DelannoyArthur <129661011+DelannoyArthur@users.noreply.github.com>
2023-11-16 17:19:22 +0100

    Update main.py

--- pychatbot-Boukef-Delannoy-Int4/main.py
+++ pychatbot-Boukef-Delannoy-Int4/main.py
@@ -1,5 +1,19 @@
 from function import*
 
 if __name__ == '__main__':
-    file = list_of_files("speeches", ".txt")
-    print(file)
+    files_names = list_of_files("speeches", ".txt")
+    print(files_names)
+    president_names = []
+    for file_name in files_names:
+        charcacter = 0
+        while file_name[charcacter] != "_":
+            charcacter = charcacter + 1
+        charcacter = charcacter + 1
+        name = ""
+        chiffres = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
+        while file_name[charcacter] not in chiffres:
+            name = name + file_name[charcacter]
+            charcacter = charcacter + 1
+            print(name)
+        president_names.append(name)
+    print(president_names)
